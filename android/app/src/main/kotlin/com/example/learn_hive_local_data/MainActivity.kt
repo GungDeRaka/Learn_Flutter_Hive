4b7c7451afb509ee43d8730f830ec4fabf19f797
GungDeRaka <aswinraka4@gmail.com>
2023-02-02 11:50:43 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/example/learn_hive_local_data/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learn_hive_local_data
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/contact.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+// isi age and name
+class Contact {
+  final String? name;
+  final int? age;
+  Contact({
+    required this.name,
+    required this.age,
+  });
+  
+}
